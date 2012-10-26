3ab96d8d8a3cba19a7fc06b2ec26b5af3e4136d2
Björn Molin <bjorn.molin@inherit.se>
2012-10-26 19:20:21 +0200

    added get by activityInstanceUuid

---
+++ inherit-service/inherit-service-rest-server/src/main/java/org/inherit/service/rest/server/ProcessInstanceDetailByActivityInstanceUuid.java
@@ -0,0 +1,26 @@
+package org.inherit.service.rest.server;
+
+import java.util.logging.Logger;
+
+import org.inherit.bonita.client.BonitaEngineServiceImpl;
+import org.inherit.service.common.domain.ProcessInstanceDetails;
+import org.inherit.service.common.util.ParameterEncoder;
+import org.restlet.resource.Post;
+import org.restlet.resource.ServerResource;
+
+
+public class ProcessInstanceDetailByActivityInstanceUuid extends ServerResource {
+
+	public static final Logger log = Logger.getLogger(ProcessInstanceDetailByActivityInstanceUuid.class.getName());
+	
+	BonitaEngineServiceImpl engine = new BonitaEngineServiceImpl();
+	
+	@Post
+	public ProcessInstanceDetails getProcessInstanceDetailsByActivityInstance() {
+		String activityInstanceUuid = ParameterEncoder.decode((String)getRequestAttributes().get("activityInstanceUuid"));
+		
+		log.fine("REST getProcessInstanceDetailsByActivityInstance with parameter activityInstanceUuid=[" + activityInstanceUuid + "]" );
+		
+		return engine.getProcessInstanceDetailsByActivityInstance(activityInstanceUuid);
+	}
+}
