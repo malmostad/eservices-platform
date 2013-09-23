1d57f442727d084a9ea7fa765bf7b57f93555fd7
Hakan Soderstrom <hs@soderstrom.se>
2013-09-23 14:35:47 +0200

    docbox 0.3: improved error handling, pdf conversion for Orbeon 4.3

---
+++ docbox/src/groovy/org/motrice/docbox/DocBoxException.groovy
@@ -0,0 +1,12 @@
+package org.motrice.docbox
+
+/**
+ * A docbox-specific runtime exception
+ */
+class DocBoxException extends RuntimeException {
+
+  DocBoxException(String message) {
+    super(message)
+  }
+
+}
