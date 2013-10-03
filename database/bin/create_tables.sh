db76e476ca9374c0bd87ebba5efe8a1c26c1e3d6
Björn Molin <bjorn.molin@inherit.se>
2013-10-03 11:50:22 +0200

    change table and sequence ownership to specified user

--- database/bin/create_tables.sh
+++ database/bin/create_tables.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if [ $# != 0 ] && [ $# != 1 ] ; then
+if [ $# != 0 ] && [ $# != 2 ] ; then
   echo 'Input database name and user or leave them blank!'
   echo 'exit...'
   exit
@@ -9,15 +9,34 @@ fi
 if [ -z "$1" ]
   then
     export DB_NAME=motricedb
+    export DB_USER=motriceuser
   else
-    export DB_NAME=$1    
+    export DB_NAME=$1
+    export DB_USER=$2    
 fi
 
-echo "./execute_file.sh $DB_NAME ../create/motrice.postgres.create.box.sql"
-su -c 'sh -c "./execute_file.sh $DB_NAME ../create/motrice.postgres.create.box.sql"'
-su -c 'sh -c "./execute_file.sh $DB_NAME ../create/motrice.postgres.create.hibernate.sql"'
-su -c 'sh -c "./execute_file.sh $DB_NAME ../create/motrice.postgres.create.mtf.sql"'
-su -c 'sh -c "./execute_file.sh $DB_NAME ../create/motrice.postgres.create.pxd.sql"'
+su -c 'sh -c "./execute_file.sh ${DB_NAME} ../create/motrice.postgres.create.box.sql"'
+su -c 'sh -c "./execute_file.sh ${DB_NAME} ../create/motrice.postgres.create.hibernate.sql"'
+su -c 'sh -c "./execute_file.sh ${DB_NAME} ../create/motrice.postgres.create.mtf.sql"'
+su -c 'sh -c "./execute_file.sh ${DB_NAME} ../create/motrice.postgres.create.pxd.sql"'
+
+#
+# postgres user owns the created tables. Change ownership to DB_USER for all tables
+#
+tables=`su -c "psql -qAt  -U postgres -d ${DB_NAME} -c 'select tablename from pg_tables where schemaname = '\''public'\'';'" postgres`
+for tbl in $tables ; do
+  echo "Change ownership on table ${tbl} to ${DB_USER}"
+  su -c "psql -c 'alter table $tbl owner to $DB_USER' $DB_NAME;" postgres
+done
+
+#
+# postgres user owns the created sequences. Change ownership to DB_USER for all sequences
+#
+sequences=`su -c "psql -qAt  -U postgres -d ${DB_NAME} -c 'SELECT c.relname FROM pg_class c WHERE c.relkind = '\''S'\'';'" postgres`
+for sequence in $sequences; do
+  echo "Change ownership on sequence ${sequence} to ${DB_USER}"
+  su -c "psql -c 'alter sequence $sequence owner to $DB_USER' $DB_NAME;" postgres
+done
 
 exit
 
