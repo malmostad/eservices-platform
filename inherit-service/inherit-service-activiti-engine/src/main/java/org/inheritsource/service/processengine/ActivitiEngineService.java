8898cc076d1a36361f69e2718bcb128f40ff34d7
pama <par.malmqvist@codeca.se>
2013-10-25 14:37:44 +0200

    activiti methods improved

--- inherit-service/inherit-service-activiti-engine/src/main/java/org/inheritsource/service/processengine/ActivitiEngineService.java
+++ inherit-service/inherit-service-activiti-engine/src/main/java/org/inheritsource/service/processengine/ActivitiEngineService.java
@@ -3,6 +3,7 @@ package org.inheritsource.service.processengine;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
@@ -14,11 +15,14 @@ import org.activiti.engine.ProcessEngine;
 import org.activiti.engine.ProcessEngineConfiguration;
 import org.activiti.engine.ProcessEngines;
 import org.activiti.engine.RepositoryService;
+import org.activiti.engine.history.HistoricTaskInstance;
 import org.activiti.engine.repository.Deployment;
 import org.activiti.engine.repository.DeploymentBuilder;
 import org.activiti.engine.repository.ProcessDefinition;
 import org.activiti.engine.task.Task;
 import org.inheritsource.service.common.domain.ActivityInstanceItem;
+import org.inheritsource.service.common.domain.ActivityInstanceLogItem;
+import org.inheritsource.service.common.domain.ActivityInstancePendingItem;
 import org.inheritsource.service.common.domain.ActivityWorkflowInfo;
 import org.inheritsource.service.common.domain.CommentFeedItem;
 import org.inheritsource.service.common.domain.DashOpenActivities;
@@ -27,6 +31,7 @@ import org.inheritsource.service.common.domain.PagedProcessInstanceSearchResult;
 import org.inheritsource.service.common.domain.ProcessDefinitionDetails;
 import org.inheritsource.service.common.domain.ProcessDefinitionInfo;
 import org.inheritsource.service.common.domain.ProcessInstanceDetails;
+import org.inheritsource.service.common.domain.UserInfo;
 
 public class ActivitiEngineService {
 
@@ -137,46 +142,9 @@ public class ActivitiEngineService {
 		}
 	}
 	
-	public List<InboxTaskItem> getUserInbox(String userId) {
-		List<InboxTaskItem> result = new ArrayList<InboxTaskItem>();
-		
-		List<Task> tasks = engine.getTaskService().createTaskQuery().taskAssignee(userId).orderByTaskCreateTime().asc().list();
-		
-		result = taskList2InboxTaskItemList(tasks);
-		
-		return result;
-	}
 	
-	private InboxTaskItem task2InboxTaskItem(Task task) {
-		InboxTaskItem item = null;
-		if (task != null) {
-			item = new InboxTaskItem();
-			item.setActivityCreated(task.getCreateTime());
-			item.setActivityDefinitionUuid(task.getTaskDefinitionKey());
-			item.setActivityLabel(task.getName());
-			item.setExpectedEndDate(task.getDueDate());
-			item.setExternalUrl("TODO from taskformdb");
-			item.setProcessActivityFormInstanceId(new Long(0)); // "TODO from taskformdb"
-			item.setProcessDefinitionUuid(task.getProcessDefinitionId());
-			item.setProcessInstanceUuid(task.getProcessInstanceId());
-			item.setProcessLabel("TODO");
-			item.setStartedByFormPath("TODO from taskformdb");
-			item.setTaskUuid(task.getId());
-		}
-		return item;
-	}
 	
-	private List<InboxTaskItem> taskList2InboxTaskItemList(List<Task> tasks) {
-		List<InboxTaskItem> result = null;
-		if (tasks != null) {
-			result = new ArrayList<InboxTaskItem>();
-			for (Task task : tasks) {
-				result.add(task2InboxTaskItem(task));
-			}
-	    }
-		return result;
-	}
-	
+
 	public static void main(String[] args) {
 		ActivitiEngineService engine = new ActivitiEngineService();
 		
@@ -192,6 +160,47 @@ public class ActivitiEngineService {
 		
 		System.exit(0);
 	}
+	
+	public List<InboxTaskItem> getUserInbox(String userId) {
+		List<InboxTaskItem> result = new ArrayList<InboxTaskItem>();
+		
+		List<Task> tasks = engine.getTaskService().createTaskQuery().taskAssignee(userId).orderByTaskCreateTime().asc().list();
+		
+		result = taskList2InboxTaskItemList(tasks);
+		
+		return result;
+	}
+	
+	private List<InboxTaskItem> taskList2InboxTaskItemList(List<Task> tasks) {
+		List<InboxTaskItem> result = null;
+		if (tasks != null) {
+			result = new ArrayList<InboxTaskItem>();
+			for (Task task : tasks) {
+				result.add(task2InboxTaskItem(task));
+			}
+	    }
+		return result;
+	}
+	
+	private InboxTaskItem task2InboxTaskItem(Task task) {
+		InboxTaskItem item = null;
+		if (task != null) {
+			item = new InboxTaskItem();
+			item.setActivityCreated(task.getCreateTime());
+			item.setActivityDefinitionUuid(task.getTaskDefinitionKey());
+			item.setActivityLabel(task.getName());
+			item.setExpectedEndDate(task.getDueDate());
+			item.setExternalUrl(""); // Will be set in TaskFormService
+			item.setProcessActivityFormInstanceId(new Long(0)); // Will be set in TaskFormService
+			item.setProcessDefinitionUuid(task.getProcessDefinitionId());
+			item.setProcessInstanceUuid(task.getProcessInstanceId());
+			item.setProcessLabel("Process Label");  // FIXME
+			item.setStartedByFormPath(""); // Will be set in TaskFormService
+			item.setTaskUuid(task.getId());
+		}
+		return item;
+	}
+	
 	
 	public Set<ProcessDefinitionInfo> getProcessDefinitions() {
 		// TODO Auto-generated method stub
@@ -212,8 +221,89 @@ public class ActivitiEngineService {
 
 	public ActivityInstanceItem getActivityInstanceItem(
 			String activityInstanceUuid) {
-		// TODO Auto-generated method stub
-		return null;
+		ActivityInstanceItem result = null;
+		
+		//activityInstanceUuid is taskId
+		Task task = engine.getTaskService().createTaskQuery().taskId(activityInstanceUuid).singleResult();
+		
+		if(task != null) {
+			result = task2ActivityInstancePendingItem(task);
+		} else {
+			HistoricTaskInstance historicTask = engine.getHistoryService().createHistoricTaskInstanceQuery().taskId(activityInstanceUuid).singleResult();
+			if(historicTask != null) {
+				result = task2ActivityInstanceLogItem(historicTask);
+			}
+		}
+		
+		return result;
+	}
+	
+	private ActivityInstanceItem task2ActivityInstancePendingItem(Task task) {
+		ActivityInstancePendingItem item = null;
+		if (task != null) {
+			item = new ActivityInstancePendingItem(); // FIXME Could be LogItem?
+			
+			item.setProcessDefinitionUuid(task.getProcessDefinitionId());
+			item.setProcessInstanceUuid(task.getProcessInstanceId());
+			item.setActivityDefinitionUuid(task.getTaskDefinitionKey());
+			item.setActivityInstanceUuid(task.getId());// FIXME getExecutionId()??
+			item.setActivityName(task.getName());
+			item.setActivityLabel(task.getName()); // FIXME Wrong mapping?
+			item.setStartDate(task.getCreateTime());
+			item.setCurrentState("FIXME"); // task.getDelegationState()?
+			item.setLastStateUpdate(null);  // FIXME  Mapping??
+			item.setLastStateUpdateByUserId("FIXME"); // FIXME: Mapping Owner / assigned ??
+			item.setStartedBy(task.getOwner()); // FIXME: Mapping correct??
+			item.setProcessActivityFormInstanceId(new Long(0)); // FIXME
+			item.setFormUrl("");
+			item.setFormDocId("");
+			item.setActivityType(99); // FIXME
+			item.setPriority(task.getPriority());
+			item.setExpectedEndDate(task.getDueDate());
+			
+			// ActivityInstancePendingItem
+			item.setCandidates(new HashSet<UserInfo>()); //FIXME
+			
+			UserInfo assignedUser = new UserInfo();
+			assignedUser.setUuid(task.getAssignee()); // FIXME
+			item.setAssignedUser(assignedUser);
+
+		}
+		return item;
+	}
+	
+	private ActivityInstanceItem task2ActivityInstanceLogItem(HistoricTaskInstance task) {
+		ActivityInstanceLogItem item = null;
+		if (task != null) {
+			item = new ActivityInstanceLogItem(); // FIXME Could be LogItem?
+			
+			item.setProcessDefinitionUuid(task.getProcessDefinitionId());
+			item.setProcessInstanceUuid(task.getProcessInstanceId());
+			item.setActivityDefinitionUuid(task.getTaskDefinitionKey());
+			item.setActivityInstanceUuid(task.getId());// FIXME getExecutionId()??
+			item.setActivityName(task.getName());
+			item.setActivityLabel(task.getName()); // FIXME Wrong mapping?
+			item.setStartDate(task.getClaimTime()); // FIXME??
+			item.setCurrentState("FIXME"); // task.getDelegationState()?
+			item.setLastStateUpdate(null);  // FIXME  Mapping??
+			item.setLastStateUpdateByUserId("FIXME"); // FIXME: Mapping Owner / assigned ??
+			item.setStartedBy(task.getOwner()); // FIXME: Mapping correct??
+			item.setProcessActivityFormInstanceId(new Long(0)); // FIXME
+			item.setFormUrl("");
+			item.setFormDocId("");
+			item.setActivityType(99); // FIXME
+			item.setPriority(task.getPriority());
+			item.setExpectedEndDate(task.getDueDate());
+			
+			// ActivityInstancelogItem
+			item.setEndDate(task.getDueDate());
+			
+			UserInfo performedByUser = new UserInfo();
+			performedByUser.setUuid(task.getAssignee()); // FIXME
+			item.setPerformedByUser(performedByUser);
+			item.setViewUrl(""); // FIXME
+		}
+		return item;
 	}
 
 	public DashOpenActivities getDashOpenActivitiesByUserId(String userId,
