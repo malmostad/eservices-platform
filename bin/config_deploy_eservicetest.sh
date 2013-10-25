f95e82fcb7a1744f6e73fa6a282b8edc8431b315
Björn Molin <bjorn.molin@inherit.se>
2013-10-25 14:40:44 +0200

    config in separate file and new deployment script with postxdb and docbox

---
+++ bin/config_deploy_eservicetest.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# ROOT of build directory
+BUILD_DIR=${HOME}/inherit-platform-gitclone/eservices-platform
+
+# ROOT of directory holding the j2ee containers
+CONTAINER_ROOT=${HOME}/inherit-platform
+
+# ROOT of Hippo jcr content repository
+CONTENT_ROOT=${CONTAINER_ROOT}/jcr-inherit-portal
+
+# Name of container roots
+BOS=BOS-5.9-Tomcat-6.0.35
+ESERVICE=hippo-eservice-tomcat-6.0.36
+KSERVICE=hippo-kservice-tomcat-6.0.36
+
+ESERVICEPATCH=eservicetest.malmo.se
+KSERVICEPATCH=kservicetest.malmo.se
+PROPERTIES_LOCAL_BEFOREPATCH=properties-local.xml.beforepatch 
+
+BOS_PORT=58080
+ESERVICE_PORT=38080
+KSERVICE_PORT=8080
+
+WITH_KSERVICES=true
