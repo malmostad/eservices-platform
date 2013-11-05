931de36fd30940e669aa97282667e8a131fb29b7
Hakan Soderstrom <hs@soderstrom.se>
2013-11-05 12:10:58 +0100

    postxdb 0.7.0

---
+++ postxdb/test/unit/org/motrice/postxdb/PostxdbServiceTests.groovy
@@ -0,0 +1,17 @@
+package org.motrice.postxdb
+
+
+
+import grails.test.mixin.*
+import org.junit.*
+
+/**
+ * See the API for {@link grails.test.mixin.services.ServiceUnitTestMixin} for usage instructions
+ */
+@TestFor(PostxdbService)
+class PostxdbServiceTests {
+
+    void testSomething() {
+        fail "Implement me"
+    }
+}
