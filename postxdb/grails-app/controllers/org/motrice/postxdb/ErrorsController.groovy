fb1dc3c6b8aecc135bb671ce1b440eb26d4e6a55
Hakan Soderstrom <hs@soderstrom.se>
2013-11-26 16:00:30 +0100

    postxdb 0.7.5 prel, no JMX connection

---
+++ postxdb/grails-app/controllers/org/motrice/postxdb/ErrorsController.groovy
@@ -0,0 +1,36 @@
+package org.motrice.postxdb
+
+import org.motrice.jmx.BasicAppManagement
+
+class ErrorsController {
+  static final EXCPREFIX = 'org.codehaus.groovy.grails.web.errors.GrailsWrappedRuntimeException: '
+  // Inject the MBean as defined in resources.groovy
+  def basicJmxManagement
+
+  /**
+   * Server errors end up here via UrlMappings
+   * The view is different in development vs production
+   */
+  def internalServerError() {
+    // Must be totally exception proof to avoid endless loop
+    try {
+      String uri = request?.forwardURI
+      String exc = request?.exception?.toString()
+      if (exc?.startsWith(EXCPREFIX)) exc = exc.substring(EXCPREFIX.size())
+      def remoteList = [request?.remoteAddr]
+      String forwHeader = request?.getHeader('X-Forwarded-For')
+      if (forwHeader) remoteList.add "forwarded for ${forwHeader}"
+      String clientHeader = request?.getHeader('Client-IP')
+      if (clientHeader) remoteList.add "client ip ${clientHeader}"
+      def remote = remoteList.join(', ')
+      basicJmxManagement.serverErrorNotification(remote, uri, exc)
+      log.error "HTTP 500: Request: ${remote}, ${uri}, exception: ${exc}"
+      def trace = request?.exception?.stackTraceText
+      if (trace) log.error "   ${trace}"
+      return [exception: request?.exception]
+    } catch (Throwable thr) {
+      log.error('HTTP500 logging caused another exception, see strack trace log file', thr)
+    }
+  }
+
+}
