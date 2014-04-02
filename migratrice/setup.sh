117300c69ef5c4ddbe862d1405e404eee84b75de
Hakan Soderstrom <hs@soderstrom.se>
2014-04-02 17:59:00 +0200

    database create script for migratrice, migratrice package for initial demodata

--- migratrice/setup.sh
+++ migratrice/setup.sh
@@ -2,4 +2,5 @@
 # Set up a development environment
 #
 export JAVA_HOME=/usr/lib/jvm/java-7-oracle
-export MIGRATRICE_CONF=/usr/local/etc/migratrice/migratrice-dev-config.properties
+#export MIGRATRICE_CONF=/usr/local/etc/migratrice/migratrice-dev-config.properties
+export MOTRICE_CONF=/usr/local/etc/motrice/motrice-apr14-config.properties
