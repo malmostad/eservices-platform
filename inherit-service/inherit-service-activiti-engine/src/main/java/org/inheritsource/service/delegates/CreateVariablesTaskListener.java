cba97d6969582c6abaa61ccd37b028c8f13a65da
Thomas <thomas.ostman at motrice.se>
2014-08-12 11:39:54 +0200

    New demo process added.

--- inherit-service/inherit-service-activiti-engine/src/main/java/org/inheritsource/service/delegates/CreateVariablesTaskListener.java
+++ inherit-service/inherit-service-activiti-engine/src/main/java/org/inheritsource/service/delegates/CreateVariablesTaskListener.java
@@ -20,30 +20,111 @@
  * e-mail: info _at_ motrice.se 
  * mail: Motrice AB, Långsjövägen 8, SE-131 33 NACKA, SWEDEN 
  * phone: +46 8 641 64 14 
- */ 
- 
+ */
+
 package org.inheritsource.service.delegates;
 
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.activiti.engine.delegate.DelegateTask;
 import org.activiti.engine.delegate.TaskListener;
+import org.inheritsource.service.form.FormEngine;
+import org.inheritsource.service.orbeon.OrbeonService;
 
 public class CreateVariablesTaskListener implements TaskListener {
 
-  
-
-    /**
+	/**
 	 * 
 	 */
+	public static final Logger log = LoggerFactory
+			.getLogger(CreateVariablesTaskListener.class.getName());
 	private static final long serialVersionUID = -3696696981293315576L;
 
-public void notify(DelegateTask delegateTask) {
-    System.out.println("message from MyTaskListener after complete of task") ;
-    System.out.println("time to grab some variables?") ;
-    System.out.println("Not implemented yet but,") ;
-    System.out.println("CreateVariablesFlowListener might be useful.") ;
-    // delegateTask.setVariableLocal("mylocalVariable", "Variable value");
-    // delegateTask.setVariable("variableFromMyTaskListener",  "valueFromMyTaskListener");
-  }
-  
-}
+	public void notify(DelegateTask delegateTask) {
+		String taskName = delegateTask.getName();
+		log.info("taskName = " + taskName);
+		log.info("CreateVariablesTaskListener called from "
+				+ delegateTask.getProcessInstanceId() + " at " + new Date());
+
+		log.warn("activityId = null     ");
+
+		String persistenceApiBaseUrl = "http://localhost:8080/exist/rest/db/orbeon-pe/fr/";
+
+		Map<String, Object> localVariables = delegateTask.getVariablesLocal();
+
+		String taskFormDataUri = (String) localVariables
+				.get(FormEngine.FORM_DATA_URI);
+		String taskFormPath = (String) localVariables
+				.get(FormEngine.FORM_DEFINITIONKEY);
+		String taskFormInstanceId = (String) localVariables
+				.get(FormEngine.FORM_INSTANCEID);
+		FormEngine.log.info("taskFormDataUri = " + taskFormDataUri);
+		log.info("taskFormPath = " + taskFormPath);
+		log.info("taskFormInstanceId" + taskFormInstanceId);
+
+		String dataUri = persistenceApiBaseUrl + taskFormPath + "/data/"
+				+ taskFormInstanceId + "/data.xml";
+		log.info("dataUri = " + dataUri);
+
+		if ((taskFormPath == null) || (taskFormInstanceId == null)) {
+			// something went wrong, or this is called from a test program
+			// where the
+			// orbeon form is not available
+			log.info("taskFormPath = " + taskFormPath);
+			log.info("taskFormInstanceId = " + taskFormInstanceId);
+		} else {
+			OrbeonService service = new OrbeonService();
+
+			HashMap<String, String> variableMap = service.getVariableMap(
+					dataUri, 2);
+			log.info("data: " + variableMap);
+
+			for (Map.Entry<String, String> entry : variableMap.entrySet()) {
+				log.info("Key : " + entry.getKey() + " Value : "
+						+ entry.getValue());
+				String newVariableNameUnfiltered = taskName + "_"
+						+ entry.getKey();
+				// String newVariableName = activityId + "_" + entry.getKey();
+				// temporary solution to get the type of the
+				// variable from the orbeon form.
+				// Use the name of the field.
+				// Only long implemented so far
+
+				// regexp to remove some unwanted characters
+				String regex = "[^a-zA-Z0-9_]"; //
+				String newVariableName = newVariableNameUnfiltered.replaceAll(
+						"-", "_").replaceAll(regex, "");
+
+				String typeDescription = "_long";
+				int indexFirst = newVariableName.length()
+						- typeDescription.length();
+				int indexLast = newVariableName.length();
+				if (indexFirst >= 0) {
+					String typeCandidate = newVariableName.substring(
+							indexFirst, indexLast);
+					if (typeDescription.equals(typeCandidate)) {
+						delegateTask.setVariable(newVariableName,
+								Long.valueOf(entry.getValue()));
+						log.info("new process variable (Long) : "
+								+ newVariableName + " created.");
+					} else {
+						delegateTask.setVariable(newVariableName,
+								entry.getValue());
+						log.info("new process variable : " + newVariableName
+								+ " created.");
+					}
+
+				} else {
+					delegateTask.setVariable(newVariableName, entry.getValue());
+					log.info("new process variable : " + newVariableName
+							+ " created.");
+				}
+			}
+		}
+	}
+} // delegateTask.setVariableLocal("mylocalVariable", "Variable value");
+
