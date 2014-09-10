264e2fc9ea08be02939ed1e06ff73441b6d7befd
Thomas <thomas.ostman at motrice.se>
2014-09-10 15:56:54 +0200

    Modified for only one tomcat.

--- bin/config_deploy_minburk.sh
+++ bin/config_deploy_minburk.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 # Setting of maven test flag
 MVN_SKIP_TEST=false
+MVN_SKIP_TEST=true
 
 # ROOT of build directory
 #BUILD_DIR=${HOME}/workspaces/motrice/pawap
@@ -12,9 +13,9 @@ CONTAINER_ROOT=${HOME}/motrice-0.6
 # ROOT of Hippo jcr content repository
 CONTENT_ROOT=${CONTAINER_ROOT}/jcr-inherit-portal
 
-TOMCAT_DIR=apache-tomcat-7.0.54
+TOMCAT_DIR=apache-tomcat-7.0.55
 TOMCAT_TGZ=${TOMCAT_DIR}.tar.gz
-TOMCAT_DOWNLOAD_URL=http://apache.mirrors.spacedump.net/tomcat/tomcat-7/v7.0.54/bin/${TOMCAT_TGZ}
+TOMCAT_DOWNLOAD_URL=http://apache.mirrors.spacedump.net/tomcat/tomcat-7/v7.0.55/bin/${TOMCAT_TGZ}
 
 #####################################################################
 # Container config 
@@ -25,50 +26,11 @@ ESERVICE_SSL=true                     # true/false
 ESERVICE_HOST=eminburk.malmo.se       # DNS name
 ESERVICE_EXTERNAL_PORT=443            # external port normally 80 or 443
 
-KSERVICE=hippo-kservice-tomcat        # PATH in CONTAINER_ROOT
-KSERVICE_SSL=true                     # true/false
-KSERVICE_HOST=kminburk.malmo.se       # DNS name
-KSERVICE_EXTERNAL_PORT=443            # external port normally 80 or 443
-
-CMSSERVICE=hippo-cmsservice-tomcat    # PATH in CONTAINER_ROOT
-CMSSERVICE_SSL=false                  #
-CMSSERVICE_HOST=kminburk.malmo.se     # DNS name
-CMSSERVICE_EXTERNAL_PORT=80           # external port normally 80 or 443
 
 ESERVICEPATCH=${ESERVICE_HOST}
-KSERVICEPATCH=${KSERVICE_HOST}
 
-ESERVICE_PORT=38080                   # internal port i.e. tomcat port
-KSERVICE_PORT=8080                    # internal port i.e. tomcat port
-CMSSERVICE_PORT=48080                 # internal port i.e. tomcat port
+ESERVICE_PORT=8080                   # internal port i.e. tomcat port
 
-WITH_KSERVICES=true
-WITH_CMSSERVICES=true
-WITH_OPENAM=true
-WITH_OPENDJ=true
+WITH_OPENDJ=false
 
-#####################################################################
-# Open AM config
-#####################################################################
-FORGEROCK_POLICY_AGENT_ZIP=tomcat_v6_agent_3.1.0-Xpress.zip
-#ORGEROCK_POLICY_AGENT_URL=file:///home/bjmo/H%C3%A4mtningar/${FORGEROCK_POLICY_AGENT_ZIP}
-FORGEROCK_POLICY_AGENT_URL=file:///home/rol/download/${FORGEROCK_POLICY_AGENT_ZIP}
-
-# OpenAM EService config
-OPENAM_POLICY_AGENT_PWD_FILE_ESERVICE=~/tmp/pwd.txt
-OPENAM_SERVER_URL_ESERVICE=https://eservicetest.malmo.se:443/openam
-OPENAM_POLICY_AGENT_ESERVICE=eserviceTestAgent
-OPENAM_REALM_ESERVICE=medbrealm
-
-# OpenAM KService config
-OPENAM_POLICY_AGENT_PWD_FILE_KSERVICE=~/tmp/pwd.txt
-OPENAM_SERVER_URL_KSERVICE=https://eservicetest.malmo.se:443/openam
-OPENAM_POLICY_AGENT_KSERVICE=kserviceTestAgent
-OPENAM_REALM_KSERVICE=kominrealm
-
-#####################################################################
-# OpenDJ config
-#####################################################################
-OPENDJ_ZIP=OpenDJ-2.6.0.zip
-OPENDJ_URL=file:///home/rol/download/${OPENDJ_ZIP}
 OPENDJ_SETUP_PROPERTIES=/usr/local/etc/motrice/opendj_setup.properties
