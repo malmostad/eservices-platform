d6c28f5eb0e063be854ec4ca1276502780ba5b2b
Thomas <thomas.ostman at motrice.se>
2014-11-19 15:34:47 +0100

    Added copyright.

--- inherit-service/inherit-service-activiti-engine/src/main/java/org/inheritsource/service/delegates/SendMail.java
+++ inherit-service/inherit-service-activiti-engine/src/main/java/org/inheritsource/service/delegates/SendMail.java
@@ -1,3 +1,28 @@
+/* == Motrice Copyright Notice == 
+ * 
+ * Motrice Service Platform 
+ * 
+ * Copyright (C) 2011-2014 Motrice AB 
+ * 
+ * This program is free software: you can redistribute it and/or modify 
+ * it under the terms of the GNU Affero General Public License as published by 
+ * the Free Software Foundation, either version 3 of the License, or 
+ * (at your option) any later version. 
+ * 
+ * This program is distributed in the hope that it will be useful, 
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of 
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the 
+ * GNU Affero General Public License for more details. 
+ * 
+ * You should have received a copy of the GNU Affero General Public License 
+ * along with this program. If not, see <http://www.gnu.org/licenses/>. 
+ * 
+ * e-mail: info _at_ motrice.se 
+ * mail: Motrice AB, Långsjövägen 8, SE-131 33 NACKA, SWEDEN 
+ * phone: +46 8 641 64 14 
+ 
+ */
+
 package org.inheritsource.service.delegates;
 
 import org.slf4j.Logger;
@@ -10,6 +35,7 @@ import javax.mail.PasswordAuthentication;
 import javax.mail.Transport;
 import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
+import javax.mail.Authenticator ; 
 
 public class SendMail {
 
@@ -49,7 +75,7 @@ public class SendMail {
 				mailprops.setProperty("mail.smtp.auth", "true");
 
 				session = Session.getInstance(mailprops,
-						new javax.mail.Authenticator() {
+						new Authenticator() {
 							protected PasswordAuthentication getPasswordAuthentication() {
 								return new PasswordAuthentication(username,
 										password);
@@ -69,7 +95,7 @@ public class SendMail {
 						// "DIGEST-MD5") ;
 					}
 					session = Session.getInstance(mailprops,
-							new javax.mail.Authenticator() {
+							new Authenticator() {
 								protected PasswordAuthentication getPasswordAuthentication() {
 									return new PasswordAuthentication(username,
 											password);
